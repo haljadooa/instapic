158364b9ab9cf7a36d4ca41e04cb04a8d0eec9f2
Hassan <haljadooa@gmail.com>
2018-06-11 04:05:16 -0700

    added partial frontend support for albums, most of the js work is done... just need a good image slider. added a standalone css file - no more pesky style tags.

--- public/js/app.js
+++ public/js/app.js
@@ -1,74 +1,111 @@
 $(document).ready(function() {
+    
     // once the "Go" button is clicked
     // download process will start
-   $('#go').on('click', function() {
+    $('#go').on('click', function() {
 
-       // url provided by the user
-       let url = document.getElementById('link').value
+        // url provided by the user
+        let url = document.getElementById('link').value,
+            isAlbum = document.getElementById('isAlbum').checked
 
-       // check if a url has been provided
-        if(url.length > 1) {
+        // check if a url has been provided
+        if (url.length > 1) {
             // check if the url is valid
-            if(validateURL(url)){
+            if (validateURL(url)) {
                 /**
                  * Ask the server to retrieve the image link
                  * Once the server has the link it'll send it back
                  */
-                axios.post('/download?url=' + url).then(function(response) {
-                    // check if the request was successful
-                    if(response.status == 200) {
-                        
-                        let content_url = response.data.data.url,
-                            content_type = response.data.data.type
-                        
-                        // check if the returned content is a photo or a video
-                        if(content_type == "image") {
-                            // set the elements attrs with the proper URLs
-                            $("#image").attr("src", content_url)
-                            $("#image-download").attr("href", content_url + "?dl=1")
-        
-                            // display preview window
-                            $("#image-preview").modal('show')
-                        }  
-                        else if (content_type == "video") {
-                            // set the elements attrs with the proper URLs
-                            $("#video").attr("src", content_url)
-                            $("#video-download").attr("href", content_url + "?dl=1")
-        
-                            // display preview window
-                            $("#video-preview").modal('show')
 
-                            // pause the video player when the modal closes
-                            $("#video-preview").on("hide.bs.modal", function() {
-                                video = document.getElementById('video')
+                if (!isAlbum) {
+                    axios.post('/download?url=' + url + "&album=0").then(function(response) {
+                        // check if the request was successful
+                        if (response.status == 200) {
 
-                                video.pause();
-                            });
-                        }    
-                    } else {
-                        alert('An unknown error occured');
-                    }
-                }).catch(function(error) {
-                    // print errors in the request to the console
-                    console.error(error);
-                });
+                            let content_url = response.data.data.url,
+                                content_type = response.data.data.type
+
+                            // check if the returned content is a photo or a video
+                            if (content_type == "image") {
+                                // set the elements attrs with the proper URLs
+                                $("#image").attr("src", content_url)
+                                $("#image-download").attr("href", content_url + "?dl=1")
+
+                                // display preview window
+                                $("#image-preview").modal('show')
+                            } else if (content_type == "video") {
+                                // set the elements attrs with the proper URLs
+                                $("#video").attr("src", content_url)
+                                $("#video-download").attr("href", content_url + "?dl=1")
+
+                                // display preview window
+                                $("#video-preview").modal('show')
+
+                                // pause the video player when the modal closes
+                                $("#video-preview").on("hide.bs.modal", function() {
+                                    video = document.getElementById('video')
+
+                                    video.pause();
+                                });
+                            }
+                        } else {
+                            alert('An unknown error occured');
+                        }
+                    }).catch(function(error) {
+                        // print errors in the request to the console
+                        console.error(error);
+                    });
+                } else {
+                    // download albums
+                    axios.post('/download?url=' + url + "&album=1").then(function(response) {
+                        
+                        // check if the request was successful
+                        if(response.status == 200) {
+                            
+                            // JSON object sent by the server
+                            let data = response.data.entry_data.PostPage[0].graphql.shortcode_media,
+                                length = data.edge_sidecar_to_children.edges.length
+                                i = 0
+                            
+                            // Array to store links
+                            // of every photo in the album
+                            let photos = []
+
+                            // cycle through JSON object
+                            // and strip the image URLs
+                            for(i=0; i < length; i++) {
+                                // push url to array
+                                photos.push(data.edge_sidecar_to_children.edges[i].node.display_url);
+                            }
+
+                            $("#album-image").attr("src", photos[0])
+
+                            $("#album-image-download").attr("href", photos[0])
+
+                            $("#album-preview").modal('show');
+
+                        }
+                    }).catch(function(error){
+                        console.error(error);
+                    });
+                }
             }
-            
         } else {
             alert("No Link Provided")
         }
-   });
+    });
 });
+
 
 // validate url
 // regex courtesy of @franciskim
 function validateURL(value) {
     let expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
     let regex = new RegExp(expression);
-    if(!regex.test(value)) {
+    if (!regex.test(value)) {
         alert('Please enter a valid URL!');
         return false;
-    }else {
+    } else {
         return true;
     }
-  }
+}
